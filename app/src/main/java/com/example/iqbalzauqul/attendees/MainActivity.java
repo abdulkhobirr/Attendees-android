e802f52f789730aa588655974d9ec4af75adba5c
iqbalzauqul <iqbal.zauqul8@gmail.com>
2017-12-01 14:24:49 +0700

    - Bug Fixes

--- app/src/main/java/com/example/iqbalzauqul/attendees/MainActivity.java
+++ app/src/main/java/com/example/iqbalzauqul/attendees/MainActivity.java
@@ -48,8 +48,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 //        setSupportActionBar(toolbar);
 
         //
