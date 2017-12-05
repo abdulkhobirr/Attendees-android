af7af2458938626aea1ab56e8d73642b334b2cd2
iqbalzauqul <iqbal.zauqul8@gmail.com>
2017-12-05 12:35:45 +0700

    - Bug Fixes

--- app/src/main/java/com/example/iqbalzauqul/attendees/DetailActivity.java
+++ app/src/main/java/com/example/iqbalzauqul/attendees/DetailActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +16,9 @@ import android.support.v7.widget.Toolbar;
 import static android.app.PendingIntent.getActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+    Menu collapsedMenu;
+    private boolean appBarExpanded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,24 @@ public class DetailActivity extends AppCompatActivity {
         String tes = getIntent().getStringExtra("nama");
         collapsingToolbar.setTitle(tes);
 
+        AppBarLayout appBarLayout = findViewById(R.id.appbar);
+        appBarExpanded = true;
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                Log.v("offset", String.valueOf(verticalOffset));
+                //  Vertical offset == 0 indicates appBar is fully  expanded.
+                if (verticalOffset < (-500)) {
+                    Log.v("offset", String.valueOf(verticalOffset));
+                    appBarExpanded = false;
+                    invalidateOptionsMenu();
+                } else {
+                    appBarExpanded = true;
+                    invalidateOptionsMenu();
+                }
+            }
+        });
 
 
 
@@ -36,4 +60,28 @@ public class DetailActivity extends AppCompatActivity {
 
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        collapsedMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (collapsedMenu != null
+                && (!appBarExpanded)) {
+            int size = collapsedMenu.size();
+            Log.v("size", String.valueOf(size));
+            //collapsed
+            collapsedMenu.add("Add")
+                    .setIcon(R.mipmap.ic_launcher_foreground)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        } else {
+            //expanded
+        }
+        return super.onPrepareOptionsMenu(collapsedMenu);
+    }
+
 }
+
